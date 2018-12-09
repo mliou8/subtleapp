b2be600b4ec6565c4ef98f7c6b09a50ef77f2af7
mliou8 <chunghanliou@gmail.com>
2018-12-09 13:07:03 -0800

    reorganize project directory

---
+++ constants/Styles.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+
+export default {
+  divider: {
+    borderBottomColor: 'black',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    paddingLeft: 10,
+    marginTop: 7,
+    marginBottom: 7,
+    justifyContent: 'flex-end',
+    width: '100%',
+  },
+  tabIconDefault: '#ccc',
+  tabIconSelected: tintColor,
+  tabBar: '#fefefe',
+  errorBackground: 'red',
+  errorText: '#fff',
+  warningBackground: '#EAEB5E',
+  warningText: '#666804',
+  noticeBackground: tintColor,
+  noticeText: '#fff',
+};
