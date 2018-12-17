b664cf7580d7a6fee9a31bf978f9a68168003200
David Martin <davidjmartin01@gmail.com>
2018-12-16 22:47:55 -0800

    add prettier config

--- app/screens/Submit/SubmitContent.js
+++ app/screens/Submit/SubmitContent.js
@@ -1,130 +1,120 @@
 import React from 'react';
-import {
-  Image,
-  StyleSheet,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  View,
-  Button,
-} from 'react-native';
+import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View, Button } from 'react-native';
 import { ImagePicker } from 'expo';
 import { Camera, Permissions } from 'expo';
 
-import TabBarIcon from 'app/components/common/TabBarIcon';
-
 export default class SubmitContent extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      display: 'initial',
-      text: '',
-      image: null,
-      hasCameraPermission: null,
-      hasCameraRollPermission: null,
-      type: Camera.Constants.Type.back,
-    }
-    this._pickImage = this._pickImage.bind(this);
-    this.renderCamera = this.renderCamera.bind(this);
-  }
-  async componentDidMount() {
-    const { status } = await Permissions.askAsync(Permissions.CAMERA);
-    this.setState({ hasCameraPermission: status === 'granted' });
-  }
-  
-  _pickImage = async () => {
-    const { rollstatus } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
-    this.setState({ hasCameraRollPermission: rollstatus === 'granted' });
-    if (this.state.hasCameraRollPermission) {
-      let result = await ImagePicker.launchImageLibraryAsync({
-        allowsEditing: true,
-        aspect: [4, 3],
-      });
-      if (!result.cancelled) {
-        this.setState({ image: result.uri });
-      }
-    }
-  };
-  
-renderCamera() {
-  const { hasCameraPermission } = this.state;
-  if (hasCameraPermission === null) {
-    return <View />;
-  } else if (hasCameraPermission === false) {
-    return <Text>No access to camera</Text>;
-  } else {
-    return (
-      <View style={{ flex: 1 }}>
-        <Camera style={{ flex: 1 }} type={this.state.type}>
-          <View
-            style={{
-              flex: 1,
-              backgroundColor: 'transparent',
-              flexDirection: 'row',
-            }}>
-            <TouchableOpacity
-              style={{
-                flex: 0.1,
-                alignSelf: 'flex-end',
-                alignItems: 'center',
-              }}
-              onPress={() => {
-                this.setState({
-                  type: this.state.type === Camera.Constants.Type.back
-                    ? Camera.Constants.Type.front
-                    : Camera.Constants.Type.back,
-                });
-              }}>
-              <Text
-                style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
-                {' '}Flip{' '}
-              </Text>
-            </TouchableOpacity>
-          </View>
-        </Camera>
-      </View>
-    )}
-  }
-  
-  render() {
-    const image = this.state.image;
+    state = {
+        display: 'initial',
+        text: '',
+        image: null,
+        hasCameraPermission: null,
+        hasCameraRollPermission: null,
+        type: Camera.Constants.Type.back,
+    };
 
-    return (
-      <View style={styles.container}>
-        <Button title="Go Back" onPress={() => this.props.resetState()}/>
-          <View style={styles.submitContainer}>
-            <Text>Submit Content</Text>
-              <TextInput
-                style={{height: 40}}
-                placeholder="Type here to translate!"
-                onChangeText={(text) => this.setState({text})}
-                />
-              <Button
-                title="Pick an image from camera roll"
-                onPress={this._pickImage}
-                />
-              {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
-              <Text style={{padding: 10, fontSize: 42}}>
-                {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
-              </Text>
-              {this.renderCamera()}
+    async componentDidMount() {
+        const { status } = await Permissions.askAsync(Permissions.CAMERA);
+        this.setState({ hasCameraPermission: status === 'granted' });
+    }
+
+    _pickImage = async () => {
+        const { rollstatus } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+        this.setState({ hasCameraRollPermission: rollstatus === 'granted' });
+        if (this.state.hasCameraRollPermission) {
+            let result = await ImagePicker.launchImageLibraryAsync({
+                allowsEditing: true,
+                aspect: [4, 3],
+            });
+            if (!result.cancelled) {
+                this.setState({ image: result.uri });
+            }
+        }
+    };
+
+    renderCamera = () => {
+        const { hasCameraPermission } = this.state;
+        if (hasCameraPermission === null) {
+            return <View />;
+        } else if (hasCameraPermission === false) {
+            return <Text>No access to camera</Text>;
+        } else {
+            return (
+                <View style={{ flex: 1 }}>
+                    <Camera style={{ flex: 1 }} type={this.state.type}>
+                        <View
+                            style={{
+                                flex: 1,
+                                backgroundColor: 'transparent',
+                                flexDirection: 'row',
+                            }}
+                        >
+                            <TouchableOpacity
+                                style={{
+                                    flex: 0.1,
+                                    alignSelf: 'flex-end',
+                                    alignItems: 'center',
+                                }}
+                                onPress={() => {
+                                    this.setState({
+                                        type:
+                                            this.state.type === Camera.Constants.Type.back
+                                                ? Camera.Constants.Type.front
+                                                : Camera.Constants.Type.back,
+                                    });
+                                }}
+                            >
+                                <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
+                                    {' '}
+                                    Flip{' '}
+                                </Text>
+                            </TouchableOpacity>
+                        </View>
+                    </Camera>
+                </View>
+            );
+        }
+    };
+
+    render() {
+        const image = this.state.image;
+
+        return (
+            <View style={styles.container}>
+                <Button title="Go Back" onPress={() => this.props.resetState()} />
+                <View style={styles.submitContainer}>
+                    <Text>Submit Content</Text>
+                    <TextInput
+                        style={{ height: 40 }}
+                        placeholder="Type here to translate!"
+                        onChangeText={text => this.setState({ text })}
+                    />
+                    <Button title="Pick an image from camera roll" onPress={this._pickImage} />
+                    {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+                    <Text style={{ padding: 10, fontSize: 42 }}>
+                        {this.state.text
+                            .split(' ')
+                            .map(word => word && '🍕')
+                            .join(' ')}
+                    </Text>
+                    {this.renderCamera()}
+                </View>
             </View>
-        </View>
-    );
-  }
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    display: 'flex',
-    flex: 1,
-    flexDirection: 'column',
-    alignContent: 'space-between',
-    justifyContent: 'center',
-  },
-  submitContainer: {
-    display: 'flex',
-    flex: 1,
-    flexDirection: 'column',
-  }
+    container: {
+        display: 'flex',
+        flex: 1,
+        flexDirection: 'column',
+        alignContent: 'space-between',
+        justifyContent: 'center',
+    },
+    submitContainer: {
+        display: 'flex',
+        flex: 1,
+        flexDirection: 'column',
+    },
 });
--- app/screens/Submit/SubmitSelectionScreen.js
+++ app/screens/Submit/SubmitSelectionScreen.js
@@ -1,78 +1,67 @@
-import React from "react";
-import {
-  Image,
-  Platform,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View
-} from "react-native";
-import { Icon } from "expo";
-import TabBarIcon from "app/components/common/TabBarIcon";
-import SubmitDating from "./SubmitDating";
-import SubmitContent from "./SubmitContent";
+import React, { Component } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Icon } from 'expo';
 
-export default class SubmitScreen extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      display: "initial"
+import SubmitDating from './SubmitDating';
+import SubmitContent from './SubmitContent';
+
+export default class SubmitScreen extends Component {
+    state = {
+        display: 'initial',
     };
-    this.resetState = this.resetState.bind(this);
-  }
-  resetState() {
-    this.setState({ display: "initial" });
-  }
 
-  render() {
-    const display = this.state.display;
-    let view;
-    if (display === "initial") {
-      view = (
-        <View style={styles.container}>
-          <TouchableOpacity
-            style={styles.buttonContainer}
-            onPress={() => {
-              this.setState({ display: "SubmitContent" });
-            }}
-          >
-            <Icon.MaterialIcons name={"add-a-photo"} size={55} />
-            <Text>Post Content</Text>
-          </TouchableOpacity>
+    resetState = () => {
+        this.setState({ display: 'initial' });
+    };
 
-          <TouchableOpacity
-            style={styles.buttonContainer}
-            onPress={() => {
-              this.setState({ display: "Dating" });
-            }}
-          >
-            <Icon.MaterialCommunityIcons name={"account-heart"} size={55} />
-            <Text>Hot Stunna</Text>
-          </TouchableOpacity>
-        </View>
-      );
-    } else if (display === "SubmitContent") {
-      view = <SubmitContent resetState={this.resetState} />;
-    } else {
-      view = <SubmitDating resetState={this.resetState} />;
+    render() {
+        const display = this.state.display;
+        let view;
+        if (display === 'initial') {
+            view = (
+                <View style={styles.container}>
+                    <TouchableOpacity
+                        style={styles.buttonContainer}
+                        onPress={() => {
+                            this.setState({ display: 'SubmitContent' });
+                        }}
+                    >
+                        <Icon.MaterialIcons name={'add-a-photo'} size={55} />
+                        <Text>Post Content</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity
+                        style={styles.buttonContainer}
+                        onPress={() => {
+                            this.setState({ display: 'Dating' });
+                        }}
+                    >
+                        <Icon.MaterialCommunityIcons name={'account-heart'} size={55} />
+                        <Text>Hot Stunna</Text>
+                    </TouchableOpacity>
+                </View>
+            );
+        } else if (display === 'SubmitContent') {
+            view = <SubmitContent resetState={this.resetState} />;
+        } else {
+            view = <SubmitDating resetState={this.resetState} />;
+        }
+        return <View style={styles.container}>{view}</View>;
     }
-    return <View style={styles.container}>{view}</View>;
-  }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    display: "flex",
-    flex: 1,
-    flexDirection: "row",
-    alignContent: "space-between",
-    justifyContent: "center"
-  },
-  buttonContainer: {
-    display: "flex",
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center"
-  }
+    container: {
+        display: 'flex',
+        flex: 1,
+        flexDirection: 'row',
+        alignContent: 'space-between',
+        justifyContent: 'center',
+    },
+    buttonContainer: {
+        display: 'flex',
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
