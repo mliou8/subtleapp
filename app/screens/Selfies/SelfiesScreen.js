e76dd2d37631ba0bd8959f87f9ec012f2745d407
Kristin <kristinnharper@gmail.com>
2019-02-02 21:46:28 -0500

    basic layout of submit and imageupload/caption all there

--- app/screens/Selfies/SelfiesScreen.js
+++ app/screens/Selfies/SelfiesScreen.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import {
-  ScrollView,
   StyleSheet,
-  View,
   SafeAreaView,
   Image,
+  Picker,
+  Keyboard,
+  View,
+  TouchableWithoutFeedback,
   TouchableOpacity,
-  Picker
+  ScrollView,
+  ImageBackground,
+  Alert
 } from 'react-native';
-
+import Modal from 'react-native-modal';
 import SelfieFeed from './subscreens/SelfieFeed';
-
-import { connect } from 'react-redux';
-
+import { ImagePicker, Permissions } from 'expo';
+import timeout from '../../util/timeout';
+// import styles from './SubmitContent.styles';
+import { newGeneralPost } from 'actions/posts/index';
+import moment from 'moment';
+import firebase from 'db/firebase';
 import db from 'db/firestore';
-
+import { connect } from 'react-redux';
+import SingleInput from 'app/components/form/SingleInput';
 import {
   Container,
   Header,
@@ -31,7 +39,11 @@ import {
   Spinner,
   Tab,
   Tabs,
-  TabHeading
+  TabHeading,
+  Form,
+  Item,
+  Input,
+  Label
 } from 'native-base';
 
 class SelfiesScreen extends React.Component {
@@ -59,81 +71,202 @@ class SelfiesScreen extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { height: 250, modalVisible: false, uploads: [], post: {} };
   }
+  async uploadPhoto() {
+    const uri = this.state.uploads[0];
+
+    this.uploadImageAsync(uri);
+  }
+  async uploadImageAsync(uri) {
+    // Why are we using XMLHttpRequest? See:
+    // https://github.com/expo/expo/issues/2402#issuecomment-443726662
+    const filename = uri.substring(uri.lastIndexOf('/') + 1);
+    const blob = await new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.onload = function() {
+        resolve(xhr.response);
+      };
+      xhr.onerror = function(e) {
+        console.log(e);
+        reject(new TypeError('Network request failed'));
+      };
+      xhr.responseType = 'blob';
+      xhr.open('GET', uri, true);
+      xhr.send(null);
+    });
+    const ref = firebase
+      .storage()
+      .ref('test/')
+      .child(`${filename}`);
+    const snapshot = await ref.put(blob);
+    blob.close();
+    const downloadURL = await snapshot.ref.getDownloadURL();
+    this.createPost(downloadURL);
+    this.props.navigation.navigate('Home');
+    // this.props.navigation.navigate('Selfies');
+  }
+
+  updateSize = height => {
+    let newHeight = height < 250 ? 250 : height;
+    this.setState({
+      height: newHeight
+    });
+  };
+
+  updateCaptionInput(caption) {
+    this.setState({ caption });
+  }
+  toggleModal = visible => {
+    this.setState({ modalVisible: visible });
+  };
+  async createPost(downloadURL) {
+    const expiryDate = new Date(
+      new Date().setFullYear(new Date().getFullYear() + 1)
+    );
+    const currUserInfo = this.props.userInfo;
+    const author = this.props.userInfo.displayName;
+    const currentTime = Date.now();
+    const datePosted = moment(currentTime).format('MMMM Do YYYY, h:mm:ss a');
+    const textToSend = JSON.stringify(this.state.caption);
+    console.log('TexttoSend ', textToSend);
+    const addPostRef = await db.collection('posts').add({
+      photoRef: downloadURL,
+      datePosted,
+      expiryDate,
+      title: this.state.title,
+      text: textToSend,
+      author,
+      location: { city: '', country: '' },
+      comments: [],
+      reactions: { likes: 0, LOLs: 0 },
+      type: 'selfie'
+    });
+    const newPostID = addPostRef.id;
+    const postData = { id: newPostID, datePosted, type: 'selfie' };
+    this.addPostToUser(postData, currUserInfo);
+  }
+  updateSize = height => {
+    let newHeight = height < 250 ? 250 : height;
+    this.setState({
+      height: newHeight
+    });
+  };
+
+  addPostToUser(postData) {
+    const currUserInfo = this.props.userInfo;
+    this.props.newGeneralPost(postData, currUserInfo);
+  }
+  pickImageFromCameraRoll = async () => {
+    this.toggleModal(false);
+    await timeout(500); // let modal transition complete
+
+    await Permissions.askAsync(Permissions.CAMERA_ROLL);
+
+    try {
+      let { uri, cancelled } = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 0.3,
+        allowsEditing: true
+      });
+      if (!cancelled) {
+        this.setState(state => {
+          return {
+            uploads: [...state.uploads, uri]
+          };
+        });
+      }
+    } catch (e) {
+      console.error('Could not get image from camera roll', e);
+    }
+  };
+  removeImage = uri => {
+    Alert.alert(
+      '',
+      'Are you sure you want to remove this picture from your post?',
+      [
+        {
+          text: 'Cancel',
+          style: 'cancel'
+        },
+        {
+          text: 'OK',
+          onPress: () => {
+            this.setState(state => {
+              return {
+                uploads: state.uploads.filter(upload => upload !== uri)
+              };
+            });
+          }
+        }
+      ]
+    );
+  };
   render() {
     return (
-      //   <ScrollView style={styles.container}>
-      <View style={{ backgroundColor: '#242424' }}>
-        {this.props.userInfo.uid ? (
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+        {/* <ScrollView style={styles.container}> */}
+        <View style={{ backgroundColor: '#242424' }}>
+          {/* {this.props.userInfo.uid ? ( */}
           <View>
-            <ScrollView>
-              <View style={{ height: 200, backgroundColor: '#242424' }}>
-                <View>
-                  <Card style={{ height: '75 %' }} transparent>
-                    <CardItem style={{ backgroundColor: '#242424' }}>
-                      <Left>
-                        <Thumbnail
-                          //   large
-                          source={{ uri: this.props.userInfo.photoURL }}
-                        />
-                      </Left>
-                      <Body>
-                        <TouchableOpacity
-                          onPress={() =>
-                            this.props.navigation.navigate('FollowersList', {
-                              type: 'following',
-                              userList: this.props.userInfo.following
-                            })
-                          }
-                        >
-                          <Text
-                            style={{ fontFamily: 'poppins', color: 'white' }}
-                          >
-                            appeal yourself
-                          </Text>
-                        </TouchableOpacity>
-                      </Body>
-                    </CardItem>
-                  </Card>
-                </View>
+            <View
+              style={{
+                height: 150,
+                backgroundColor: '#242424',
+                display: 'flex'
+              }}
+            >
+              <Thumbnail
+                //   large
+                source={{ uri: this.props.userInfo.photoURL }}
+              />
 
-                <View>
-                  <Tabs tabBarUnderlineStyle={{ backgroundColor: 'white' }}>
-                    <Tab
-                      tabStyle={{ backgroundColor: '#242424' }}
-                      heading={
-                        <TabHeading style={{ backgroundColor: '#242424' }}>
-                          <Icon name="ios-camera" style={{ color: 'white' }} />
-                          <Text
-                            style={{ fontFamily: 'poppins', color: 'white' }}
-                          >
-                            Photos
-                          </Text>
-                        </TabHeading>
-                      }
-                    >
-                      <SelfieFeed />
-                    </Tab>
-                    <Tab
-                      heading={
-                        <TabHeading style={{ backgroundColor: '#242424' }}>
-                          <Icon
-                            name="plus"
-                            style={{ color: 'white' }}
-                            type="Feather"
-                          />
-                          <Text
-                            style={{ fontFamily: 'poppins', color: 'white' }}
-                          >
-                            Submit
-                          </Text>
-                        </TabHeading>
-                      }
-                    />
-                  </Tabs>
-                </View>
-              </View>
+              <Form>
+                <Item
+                  style={{
+                    borderColor: 'transparent'
+                  }}
+                  floatingLabel
+                >
+                  <Label style={{ fontFamily: 'poppins', color: 'white' }}>
+                    Appeal yourself
+                  </Label>
+                  <Input
+                    style={{ fontFamily: 'poppins', color: 'white' }}
+                    onChangeText={caption => this.updateCaptionInput(caption)}
+                    value={this.state.caption}
+                  />
+                </Item>
+              </Form>
+            </View>
+            <View
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                alignContent: 'center',
+                justifyContent: 'space-around'
+              }}
+            >
+              <Button
+                iconLeft
+                style={{ backgroundColor: '#242424' }}
+                onPress={this.pickImageFromCameraRoll}
+              >
+                <Icon name="ios-camera" style={{ color: 'white' }} />
+                <Text>Photos</Text>
+              </Button>
+              <Button
+                iconLeft
+                style={{ backgroundColor: '#242424' }}
+                onPress={() => this.uploadPhoto()}
+              >
+                <Icon name="plus" type="Feather" style={{ color: 'white' }} />
+                <Text style={{ fontFamily: 'poppins', color: 'white' }}>
+                  Submit
+                </Text>
+              </Button>
+            </View>
+            <ScrollView>
               <View>
                 <Card style={{ padding: 3 }}>
                   <CardItem cardBody>
@@ -198,10 +331,11 @@ class SelfiesScreen extends React.Component {
               </View>
             </ScrollView>
           </View>
-        ) : (
-          <Spinner color="blue" />
-        )}
-      </View>
+          {/* ) : (
+            <Spinner color="blue" />
+          )} */}
+        </View>
+      </TouchableWithoutFeedback>
     );
   }
 }
@@ -210,7 +344,6 @@ const mapStateToProps = (state, ownProps) => {
   return {
     ...state,
     userInfo: state.login.userInfo,
-    userInfo: state.login.userInfo,
     profile: state.profile,
     login: state.login
   };
@@ -218,14 +351,12 @@ const mapStateToProps = (state, ownProps) => {
 
 const mapDispatchToProps = (dispatch, ownProps) => {
   return {
-    fetchUser: uid => {
-      dispatch(fetchUser(uid));
-    },
-    fetchUserProfileInfo: uid => {
-      dispatch(fetchUserProfileInfo(uid));
+    newGeneralPost: (postData, currUserInfo) => {
+      dispatch(newGeneralPost(postData, currUserInfo));
     }
   };
 };
+
 export default connect(
   mapStateToProps,
   mapDispatchToProps
